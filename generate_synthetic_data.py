58373cab16ba4b1ec90c523acd143c961767c82b
Devvjiit Bhuyan <Dev.bhuyan.professional@gmail.com>
2024-04-11 03:46:14 +0530

    Add files via upload

---
+++ generate_synthetic_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 16:33:34 2024
+
+@author: dev
+"""
+
+from sklearn.datasets import make_classification
+import pandas as pd
+from random import random
+
+NUM_CLASSES = 25
+
+class_weights = [random() for _ in range(NUM_CLASSES)]
+
+X, y = make_classification(n_samples=100000,
+                           n_features=200,
+                           n_informative=35,
+                           n_redundant=65,
+                           n_classes=NUM_CLASSES,
+                           weights=class_weights,
+                           random_state=42)
+
+df = pd.DataFrame(X.round(3), columns=[f"V_{i}" for i in range(200)])
+df['Class'] = y
+
+df.to_csv('./datasets/synthetic.csv', index=False)
