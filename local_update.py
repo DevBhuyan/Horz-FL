e300033fd7fa23083207c9e088cebf338075a77e
Devvjiit Bhuyan <Dev.bhuyan.professional@gmail.com>
2024-06-13 02:48:44 +0530

    Documented

--- local_update.py
+++ local_update.py
@@ -13,6 +13,40 @@ from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_sc
 # Implementation for FedAvg clients
 
 class user_avg():
+    """
+    Class representing a user in the Federated Averaging (FedAvg) setup.
+
+    Attributes:
+    - device (torch.device): Device (CPU or GPU) on which the model operates.
+    - local_model (nn.Module): Local copy of the global model for this user.
+    - id (int): Identifier for the user.
+    - batch_size (int): Batch size for training.
+    - learning_rate (float): Learning rate for training.
+    - local_iters (int): Number of local training iterations.
+    - criterion (torch.nn.CrossEntropyLoss): Loss function for training.
+    - optimizer (torch.optim.SGD): Optimizer used for training.
+    - trainloader (DataLoader): DataLoader for training data.
+    - val_loader (DataLoader): DataLoader for validation data.
+    - testloader (DataLoader): DataLoader for test data.
+    - trainloaderfull (DataLoader): DataLoader for full training data (used for evaluation).
+    - train_samples (int): Number of samples in the training set.
+    - val_samples (int): Number of samples in the validation set.
+    - test_samples (int): Number of samples in the test set.
+
+    Methods:
+    - df_to_tensor(dataframe): Converts a pandas DataFrame to PyTorch tensors.
+    - set_parameters(model): Sets parameters of the local model to those of a given global model.
+    - get_parameters(): Retrieves parameters of the local model.
+    - clone_model_paramenter(param, clone_param): Clones parameters of the local model.
+    - get_updated_parameters(): Retrieves updated parameters after training.
+    - update_parameters(new_params): Updates parameters of the local model with new parameters.
+    - train_error_and_loss(global_model): Computes training accuracy and loss.
+    - validation(global_model): Performs validation and computes metrics.
+    - test(global_model): Performs test and computes metrics.
+    - train(): Performs local training iterations.
+
+    """
+
     def __init__(self,
                  args,
                  device,
@@ -22,6 +56,18 @@ class user_avg():
                  test_df,
                  global_model
                  ):
+        """
+        Initialize a user in the FedAvg setup.
+
+        Parameters:
+        - args (object): Object containing arguments such as batch_size, lr, and local_iters.
+        - device (torch.device): Device (CPU or GPU) on which the model operates.
+        - id (int): Identifier for the user.
+        - train_df (pd.DataFrame): DataFrame containing training data.
+        - val_df (pd.DataFrame): DataFrame containing validation data.
+        - test_df (pd.DataFrame): DataFrame containing test data.
+        - global_model (nn.Module): Global model shared among users.
+        """
 
         self.device = device
         self.local_model = copy.deepcopy(global_model)
@@ -61,32 +107,83 @@ class user_avg():
 
     # Convert DataFrames to tensors
     def df_to_tensor(self, dataframe):
+        """
+        Convert a pandas DataFrame to PyTorch tensors.
+
+        Parameters:
+        - dataframe (pd.DataFrame): DataFrame containing features and labels.
+
+        Returns:
+        - torch.tensor: Tensor of features.
+        - torch.tensor: Tensor of labels.
+        """
         features = dataframe.drop(columns='Class').values
         labels = dataframe['Class'].values
         return torch.tensor(features, dtype=torch.float32), torch.tensor(labels, dtype=torch.long)
 
     def set_parameters(self, model):
+        """
+        Set parameters of the local model to those of a given global model.
+
+        Parameters:
+        - model (nn.Module): Global model whose parameters will be copied.
+        """
         for param, glob_param in zip(self.local_model.parameters(), model.parameters()):
             param.data = glob_param.data.clone()
 
     def get_parameters(self):
+        """
+        Retrieve parameters of the local model.
+
+        Returns:
+        - torch.parameters: Parameters of the local model.
+        """
         for param in self.local_model.parameters():
             param.detach()
         return self.local_model.parameters()
 
     def clone_model_paramenter(self, param, clone_param):
+        """
+        Clone parameters of the local model.
+
+        Parameters:
+        - param (torch.parameters): Parameters of the local model.
+        - clone_param (torch.parameters): Cloned parameters of the local model.
+        """
         for param, clone_param in zip(param, clone_param):
             clone_param.data = param.data.clone()
         return clone_param
 
     def get_updated_parameters(self):
+        """
+        Retrieve updated parameters after training.
+
+        Returns:
+        - torch.parameters: Updated parameters of the local model.
+        """
         return self.local_weight_updated
 
     def update_parameters(self, new_params):
+        """
+        Update parameters of the local model with new parameters.
+
+        Parameters:
+        - new_params (torch.parameters): New parameters to update.
+        """
         for param, new_param in zip(self.local_model.parameters(), new_params):
             param.data = new_param.data.clone()
 
     def train_error_and_loss(self, global_model):
+        """
+        Compute training accuracy and loss.
+
+        Parameters:
+        - global_model (nn.Module): Global model for comparison.
+
+        Returns:
+        - float: Accuracy of the local model on the training set.
+        - float: Loss of the local model on the training set.
+        """
 
         # Set the model to evaluation mode
         self.local_model.eval()
@@ -117,6 +214,19 @@ class user_avg():
         return accuracy, train_loss
 
     def validation(self, global_model):
+        """
+        Perform validation and compute metrics.
+
+        Parameters:
+        - global_model (nn.Module): Global model for comparison.
+
+        Returns:
+        - float: Accuracy of the local model on the validation set.
+        - float: Loss of the local model on the validation set.
+        - float: Precision of the local model on the validation set.
+        - float: Recall of the local model on the validation set.
+        - float: F1 score of the local model on the validation set.
+        """
         # Set the model to evaluation mode
         self.local_model.eval()
         self.update_parameters(global_model)
@@ -154,6 +264,19 @@ class user_avg():
         return accuracy, validation_loss, precision, recall, f1
 
     def test(self, global_model):
+        """
+        Perform test and compute metrics.
+
+        Parameters:
+        - global_model (nn.Module): Global model for comparison.
+
+        Returns:
+        - float: Accuracy of the local model on the test set.
+        - float: Loss of the local model on the test set.
+        - float: Precision of the local model on the test set.
+        - float: Recall of the local model on the test set.
+        - float: F1 score of the local model on the test set.
+        """
         # Set the model to evaluation mode
         self.local_model.eval()
         self.update_parameters(global_model)
